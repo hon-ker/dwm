9ae4fff1bc6677a0c7245e7f9aae4f2a3863cbc7
clay <1838237623@qq.com>
2023-06-14 20:29:16 +0800

    Aplay change keybinding

--- config.h
+++ config.h
@@ -100,10 +100,16 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
+  /* modifier                     key        function        argument */
+  // 系统级 亮度，音量等
+  {ControlMask,                 XK_F2,      spawn,            SHCMD("~/.dwm/scripts/setVol.sh down")},
+  {ControlMask,                 XK_F3,      spawn,            SHCMD("~/.dwm/scripts/setVol.sh up")},
+  {ControlMask,                 XK_F6,      spawn,            SHCMD("~/.dwm/scripts/setBacklight.sh down")},
+  {ControlMask,                 XK_F7,      spawn,            SHCMD("~/.dwm/scripts/setBacklight.sh up")},
+  // 自定义快捷键
   { MODKEY,                     XK_d,       spawn,            SHCMD("rofi -show drun") },
   { MODKEY,                     XK_c,       spawn,            SHCMD("google-chrome-stable") },
   { MODKEY,                     XK_Return,  spawn,            SHCMD("alacritty")},
@@ -111,40 +117,49 @@ static const Key keys[] = {
 
   { MODKEY,                     XK_q,       killclient,     {0} },
   { MODKEY|ShiftMask,           XK_Return,  zoom,           {0} },
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+
+  // 打开关闭状态栏
+  { MODKEY,                     XK_m,       togglebar,      {0} },
+
+  // grid模式和切换
+  //{ MODKEY,                     XK_a,       setlayout,      {.v = &layouts[3]} },
+  //{ MODKEY|ControlMask,         XK_a,       setlayout,      {.v = &layouts[0]} },
+  // 改变mask窗口大小
+  { MODKEY,                     XK_comma,     setmfact,       {.f = -0.05} },
+  { MODKEY,                     XK_period,    setmfact,       {.f = +0.05} },
+
+  // 全屏切换
+//  { MODKEY,                     XK_f,      togglefullscr,  {0} },
+
+  { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+  { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+  { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+  { MODKEY|ShiftMask,             XK_i,      incnmaster,     {.i = -1 } },
+  { MODKEY,                       XK_Tab,    view,           {0} },
+  { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+  //{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+  //{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+  { MODKEY,                       XK_space,  setlayout,      {0} },
+  { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+  { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+  { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+  { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+  { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+  { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+  { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+  { MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+  { MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+  { MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+  TAGKEYS(                        XK_1,                      0)
+    TAGKEYS(                        XK_2,                      1)
+    TAGKEYS(                        XK_3,                      2)
+    TAGKEYS(                        XK_4,                      3)
+    TAGKEYS(                        XK_5,                      4)
+    TAGKEYS(                        XK_6,                      5)
+    TAGKEYS(                        XK_7,                      6)
+    TAGKEYS(                        XK_8,                      7)
+    TAGKEYS(                        XK_9,                      8)
+    { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
