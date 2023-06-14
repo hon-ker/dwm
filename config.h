56b6c05c39fa29649730d49166b098ea2fdcecf7
clay <1838237623@qq.com>
2023-06-14 20:33:02 +0800

    Aplay patch fullscreen

--- config.h
+++ config.h
@@ -129,8 +129,8 @@ static const Key keys[] = {
   { MODKEY,                     XK_period,    setmfact,       {.f = +0.05} },
 
   // 全屏切换
-//  { MODKEY,                     XK_f,      togglefullscr,  {0} },
-
+//{ MODKEY,                     XK_f,      togglefullscr,  {0} },
+{ MODKEY,             XK_f,      fullscreen,     {0} },
   { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
   { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
   { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
